2347a676815e2a4f38b11388fa82f62fa3d50092
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-22 19:11:09 -0500

    commited exercise23

---
+++ exercise23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number3 = 1;
+
+while (number3 <= number)
+{
+    Console.WriteLine(number3 * number3 * number3);
+    number3++;
+}
+return (number);
